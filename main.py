4ba44e889ccd77c40b23f9af3b8545af9fd1e036
Gokul1011-gk <gokul.t6936@gmail.com>
2025-10-02 19:57:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 import requests
 import os
 from dotenv import load_dotenv
@@ -11,28 +11,21 @@ API_KEY = os.getenv("NEWSDATA_KEY")
 BASE_URL = "https://newsdata.io/api/1/news"
 
 @app.get("/trending")
-def get_filtered_news(
-    category: str = Query(None, description="News category"),
-    country: str = Query(None, description="Country code (e.g., 'in', 'us')"),
-    page: int = Query(1, ge=1, description="Page number"),
-    limit: int = Query(10, ge=1, le=50, description="Max articles to return")
-):
+def get_global_news():
     params = {
         "apikey": API_KEY,
-        "language": "en",
-        "page": page,
-        "category": category,
-        "country": country
+        "language": "en"
     }
 
     try:
         response = requests.get(BASE_URL, params=params)
         response.raise_for_status()
         data = response.json()
-        articles = data.get("results", [])[:limit]
+        articles = data.get("results", [])
         return {"articles": articles}
     except requests.exceptions.RequestException as e:
         return {"error": str(e)}
+
 
 
 
